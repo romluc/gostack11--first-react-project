4765446f9558992e52efa0758f66cda67a91239e
romluc <romulofilhofilho@gmail.com>
2020-05-08 15:02:47 -0300

    Create Dashboard and Repository FCs and routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" component={Dashboard} />
+    <Route />
+  </Switch>
+);
+
+export default Routes;
