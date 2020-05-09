1f90de479ad551e44b6cca54a45d94396d1cb127
romluc <romulofilhofilho@gmail.com>
2020-05-09 16:34:23 -0300

    Finish styling Dashboard component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logo from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Github repos!</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer logo" />
+      <Title>
+        Explore Github repos{' '}
+        <span
+          role="img"
+          aria-label="bitmojis of eye looking through a telescope"
+        >
+          🔭👀
+        </span>
+      </Title>
+
+      <Form>
+        <input placeholder="Enter repo name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/44209758?s=400&u=02034e523c0e39f290c57e2c093b843c2b9dfb0c&v=4"
+            alt="github Romulo Lucena Castro profile"
+          />
+          <div className="text-container">
+            <strong>romluc/huntweb</strong>
+            <p>React app consuming a Rocketseat&apos;s heroku products api</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/44209758?s=400&u=02034e523c0e39f290c57e2c093b843c2b9dfb0c&v=4"
+            alt="github Romulo Lucena Castro profile"
+          />
+          <div className="text-container">
+            <strong>romluc/huntweb</strong>
+            <p>React app consuming a Rocketseat&apos;s heroku products api</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/44209758?s=400&u=02034e523c0e39f290c57e2c093b843c2b9dfb0c&v=4"
+            alt="github Romulo Lucena Castro profile"
+          />
+          <div className="text-container">
+            <strong>romluc/huntweb</strong>
+            <p>React app consuming a Rocketseat&apos;s heroku products api</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,7 +1,95 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
   text-align: center;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 40px;
+  max-width: 700px;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0px 0px 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #009090;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#009090')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    padding: 24px;
+    width: 100%;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    .text-container {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        margin-top: 4px;
+        color: #a8a8a8;
+        font-size: 18px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd5;
+    }
+  }
 `;
