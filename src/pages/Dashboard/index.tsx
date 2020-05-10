36e8ef09f1e3a530880c210d6368b5780e83dd2f
romluc <romulofilhofilho@gmail.com>
2020-05-09 21:44:23 -0300

    Finish Dashboard & get method from api using axios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
 import logo from '../../assets/logo.svg';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError(`Blank field. Enter author/repo's name.`);
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Found an error while searching the repo... =(');
+    }
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer logo" />
@@ -17,48 +52,32 @@ const Dashboard: React.FC = () => {
         </span>
       </Title>
 
-      <Form>
-        <input placeholder="Enter repo name" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Enter repo name"
+        />
         <button type="submit">Search</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/44209758?s=400&u=02034e523c0e39f290c57e2c093b843c2b9dfb0c&v=4"
-            alt="github Romulo Lucena Castro profile"
-          />
-          <div className="text-container">
-            <strong>romluc/huntweb</strong>
-            <p>React app consuming a Rocketseat&apos;s heroku products api</p>
-          </div>
+        {repositories.map((repository) => (
+          <a href="teste" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div className="text-container">
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/44209758?s=400&u=02034e523c0e39f290c57e2c093b843c2b9dfb0c&v=4"
-            alt="github Romulo Lucena Castro profile"
-          />
-          <div className="text-container">
-            <strong>romluc/huntweb</strong>
-            <p>React app consuming a Rocketseat&apos;s heroku products api</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars2.githubusercontent.com/u/44209758?s=400&u=02034e523c0e39f290c57e2c093b843c2b9dfb0c&v=4"
-            alt="github Romulo Lucena Castro profile"
-          />
-          <div className="text-container">
-            <strong>romluc/huntweb</strong>
-            <p>React app consuming a Rocketseat&apos;s heroku products api</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   display: flex;
   margin-top: 40px;
   max-width: 700px;
@@ -22,6 +26,14 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0px 0px 5px;
     color: #3a3a3a;
+    background-color: #fff;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border: 1px solid tomato;
+        border-right: 0;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -42,6 +54,14 @@ export const Form = styled.form`
       background: ${shade(0.2, '#009090')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 12px;
+  font-weight: bold;
+  font-size: 14px;
 `;
 
 export const Repositories = styled.div`
@@ -74,7 +94,8 @@ export const Repositories = styled.div`
     }
 
     .text-container {
-      margin-left: 16px;
+      margin: 0 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
